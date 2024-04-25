c997641d353946688acee77ded51b771d72a29ac
rohimouktik <151356835+rohimouktik@users.noreply.github.com>
2024-04-25 20:25:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,84 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH = 'potatoes.h5'
+
+# Load your trained model
+model = load_model(MODEL_PATH)
+model.make_predict_function()          # Necessary
+# print('Model loaded. Start serving...')
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+#from keras.applications.resnet50 import ResNet50
+#model = ResNet50(weights='imagenet')
+#model.save('')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(256, 256))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x, mode='caffe')
+
+    preds = model.predict(x)
+    class_names = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+    
+    # Since you have only one prediction, you can directly access it
+    single_prediction = class_names[np.argmax(preds[0])]
+    os.remove(img_path)
+    return single_prediction
+
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        single_prediction = model_predict(file_path, model)
+
+        # Process your result for human
+        result = single_prediction # Example: getting the index of the maximum value
+        return result
+    return None
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
